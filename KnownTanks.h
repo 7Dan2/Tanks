9f79482b71d6ace23185dd23e2a0e683c8e19be7
7dan2 <unic.norton@gmail.com>
2022-03-21 10:40:33 +0100

    work on menu in main.cpp

--- KnownTanks.h
+++ KnownTanks.h
@@ -1,14 +1,71 @@
-#ifndef KNOWNTANKS_H
-#define KNOWNTANKS_H
-
+#ifndef MAINE_H_INCLUDED
+#define MAINE_H_INCLUDED
 
 class KnownTanks
 {
-    public:
-        KnownTanks();
-        virtual ~KnownTanks();
-    protected:
-    private:
+private:
+    /**< Valeurs pour chaque centimetre des citernes 30 000 litres */
+    static const int tankValuesPerCm_30k[250];
+
+    static const int tankValuesPerCm_20k[250];
+
+    // Variable stockant la valeur du nombre d'elements du tableau
+    int tankValuesPerCm_30kLength;
+
+    // variable de stockage de la hauteur mesuree brut;
+    float currentRawHeight;
+
+    // Valeur arrondie a l'entier superieur
+    float ceiledHeight;
+
+    // La valeur arrondie est diminuŽe de -1 pour ensuite servir de parametre de recherche dans le tableau des valeurs
+    int floorCorrespondingValue;
+
+    // Choix de la citerne
+    int tankChoice;
+
+    // variables fixant les contenances maximum de chaque citerne
+    int static const maxContentTankOne_20k = 19170;
+
+    int static const maxContentTankOne_30k = 30231;
+    int static const maxContentTankTwo_30k = 29231;
+
+public:
+    //Setters
+
+    // Enregistrement de la valeur brut "aValue" dans "currentRawHeight"
+    void setRawHeight( float aValue );
+
+    // Enregistrement de la valeur arrondie supérieure 'aRawHeight" dans "ceiledHeight"
+    void setCeiledHeight( float aRawHeight );
+
+    void setFloorCorrespondingValue( int aCeiledHeigt );
+
+    void setCurrentTank( int aValue );
+
+    //Getters
+    float getCurrentRawHeight();
+
+    int getCeiledHeight();
+
+    int getFloorCorrespondingValue();
+
+    int getSelectedTank();
+
+    // Methods
+
+    int getFuelVolumeRemain( int aFloorValue );
+
+    int toCeil( float aRawValue );
+
+    int toFloor( int aCeiledValue );
+
+    int getAllArrayValues();
+
+    // Constructeur
+    KnownTanks();
+
 };
 
-#endif // KNOWNTANKS_H
+
+#endif // MAINE_H_INCLUDED
--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
+#include "KnownTanks.h"
+#include "ExoticTanks.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    cout << "Welcome in Tanks app" << "\n\n";
+    cout << "Choose a known tank with 1 or 2" << "\n";
+    cout << "Or a personalized tank with your values => choose 3" << "\n";
+    cout << "1 - 20 000 liters tank" << "\n";
+    cout << "2 - 30 000 liters tank" << "\n";
+    cout << "3 - Your customised tank" << "\n";
     return 0;
 }
